af85f58d91918b44f15047edb854fd4200770016
Bartłomiej Mazur <ab.mazur@gmail.com>
2019-09-06 16:49:43 +0200

    Login to Admin

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const login = "admin";
+const password = "123";
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', (req, res)=> {
+  res.render('login', { title: 'Logowanie'})
+})
+
+router.post('/login', (req, res)=> {
+  console.log(req.body);
+  const body = req.body;
+  if (body.login === login && body.password === password) {
+    res.redirect('/admin')
+  } else {
+    res.redirect('/login')
+  }
+
+})
 
 module.exports = router;
