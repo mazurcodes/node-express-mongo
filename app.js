cc2224a1ee69a33d25a484f02ffae6d1307d2dfe
Bartłomiej Mazur <ab.mazur@gmail.com>
2019-09-06 14:21:07 +0200

    Basic Routes for app

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const newsRouter = require('./routes/news');
+const quizRouter = require('./routes/quiz');
+const adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/news', newsRouter);
+app.use('/quiz', quizRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
