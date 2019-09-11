ed2d4eeb061008623d1cd61aaa0b7478b6da5708
Bartłomiej Mazur <ab.mazur@gmail.com>
2019-09-11 12:48:03 +0200

    Quiz done

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,22 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Quiz = require('../models/quiz');
+const Quiz = require("../models/quiz");
 
 /* GET home page. */
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  const show = !req.session.vote;
   Quiz.find({}, (err, data) => {
-    res.render('quiz', { title: 'Quiz', data});
-  })
+    const votesTotal = data.reduce((total, item) => total + item.vote, 0);
+    res.render("quiz", { title: "Quiz", data, show, votesTotal });
+  });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const _id = req.body.quiz;
-  Quiz.findOne({_id}, (err, data) => {
-    data.vote += 1;
-    data.save(() => {
-      res.redirect('/quiz');
-    });
-  });
-})
+  if (_id) {
+    Quiz.findOne({ _id }, (err, data) => {
+      data.vote += 1;
+      req.session.vote = 1;
+      data.save(() => {
+        res.redirect("/quiz");
+      });
+    })
+  } else {
+    res.redirect("/quiz");
+  }
+});
 
 module.exports = router;
