6141baca778f7a5526c73be216f93ab2b0645242
Bartłomiej Mazur <ab.mazur@gmail.com>
2019-09-12 11:49:45 +0200

    Some fixes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: "",
+  db: "mongodb+srv://adminDB:pNSASpt8FtuGMtfq@cluster0-3zlna.mongodb.net/test?retryWrites=true&w=majority",
   keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000
 };
